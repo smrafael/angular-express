07ac6f737bb96f7e3cde7c4258f57473f588da31
Rafael Mendes <rafael.sm1990@gmail.com>
2017-02-15 16:54:41 -0300

    improving permissions middleware

--- src/server/utils/permissions.js
+++ src/server/utils/permissions.js
@@ -3,7 +3,7 @@ var jwt = require('jsonwebtoken');
 // Roles are not being verified yet
 module.exports.hasRoles = function(roles) {
   return function(req, res, next) {
-    verifyToken(req, function(token) {
+    verifyToken(req, function(decodedToken) {
       next();
     }, function(err) {
       res.status(401).send(err);
@@ -17,8 +17,9 @@ module.exports.hasRoles = function(roles) {
       jwt.verify(token, 'secret', function(err, decoded) {
         if (err) {
           error(err.name+": "+err.message);
+          return;
         }
-        callback(token);
+        callback(decoded);
       });
     } else {
       error('undefined token');
